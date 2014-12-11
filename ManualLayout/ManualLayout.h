1fa6087934e8897848392f1a662af06d41ae5536
Sam Morrison <sam@sam.vg>
2014-12-10 18:34:09 -0800

    Initial Commit

---
+++ ManualLayout/ManualLayout.h
@@ -0,0 +1,19 @@
+//
+//  ManualLayout.h
+//  ManualLayout
+//
+//  Created by Sam Morrison on 12/10/14.
+//  Copyright (c) 2014 Patreon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ManualLayout.
+FOUNDATION_EXPORT double ManualLayoutVersionNumber;
+
+//! Project version string for ManualLayout.
+FOUNDATION_EXPORT const unsigned char ManualLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManualLayout/PublicHeader.h>
+
+
